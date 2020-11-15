8b20edbc8c42c8455763219ef8e0cb1b10c81c17
Ana Garich <ana.garich@mail.utoronto.ca>
2020-11-15 17:07:33 -0500

    Added print functions to the presenter relating to methods in ScheduleSystem.

--- phase1/src/Controller/Presenter.java
+++ phase1/src/Controller/Presenter.java
@@ -86,5 +86,61 @@ public class Presenter {
         System.out.println("Please check that username or password is non-empty and username is not already taken.");
     }
 
+    /**
+     * Displays message reflecting whether a speaker was successfully changed.
+     * @param changeSpeakerOutput output of the ScheduleSystem.changeSpeaker() method.
+     */
+    public void printChangeSpeakerMessage(int changeSpeakerOutput){
+        switch (changeSpeakerOutput){
+            case 0:
+                System.out.println("Speaker changed successfully.");
+                break;
+            case 1:
+                System.out.println("Speaker is already booked at this time.");
+                break;
+            case 2:
+                System.out.println("This person is not a speaker.");
+                break;
+            case 3:
+                System.out.println("This user does not exist.");
+                break;
+        }
+    }
+
+    /**
+     * Displays message reflecting whether an event was successfully created and if not what the issue is.
+     * @param scheduleEventMessage is the output of the ScheduleSystem.createEvent() method.
+     */
+    public void printScheduleEventMessage(int scheduleEventMessage) {
+        switch (scheduleEventMessage) {
+            case 0:
+                System.out.println("Room is already booked for this timeslot.");
+                break;
+            case 1:
+                System.out.println("Speaker is already booked for this timeslot.");
+                break;
+            case 2:
+                System.out.println("This event name has already been taken.");
+                break;
+            case 3:
+                System.out.println("Event successfully created.");
+                break;
+        }
+    }
+
+    /**
+     * Displays message reflecting whether a room was successfully added or a message indicating error.
+     * @param addRoomMessage output from the ScheduleSystem.addRoom() method
+     */
+    public void printAddRoomMessage(int addRoomMessage){
+        switch (addRoomMessage){
+            case 0:
+                System.out.println("Room successfully added.");
+                break;
+            case 1:
+                System.out.println("Room already exists in System.");
+                break;
+        }
+    }
 
 }
