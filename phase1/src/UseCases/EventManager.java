53ad9c36633cac7c3979af2c1d71f6496d93d692
Ana Garich <ana.garich@mail.utoronto.ca>
2020-11-15 19:22:53 -0500

    Added javadoc. Changed the return type of signUp method to boolean.

--- phase1/src/UseCases/EventManager.java
+++ phase1/src/UseCases/EventManager.java
@@ -9,17 +9,43 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.util.ArrayList;
 
+/**
+ * Manages and stores events in this tech conference system.
+ */
 public class EventManager implements Serializable {
     private ArrayList<Event> events;
 
+    /**
+     * Constructs an instance of EventManager with an empty Arraylist of Events.
+     */
     public EventManager(){
         this.events = new ArrayList<Event>();
     }
 
+    /**
+     * This method returns an ArrayList of events.
+     * @return an ArrayList<Event> representing the events occurring at the tech conference.
+     */
     public ArrayList<Event> getEvents() {
         return events;
     }
 
+    /**
+     * This method creates an event instance when the following conditions are satisfied:
+     * 1) The desired room is free
+     * 2) The selected speaker is free.
+     * 3) The event name is available.
+     * @param title the desired event name.
+     * @param speaker the desired speaker username.
+     * @param year the year the event starts.
+     * @param month the month the event starts.
+     * @param day the day the event starts.
+     * @param hour the hour the event starts.
+     * @param minute the minute the event starts.
+     * @param room the room id of the desired room where the event takes place.
+     * @param capacity
+     * @return an integer signifying whether the event was successfully created or an error message.
+     */
     public int createEvent(String title, String speaker, int year, String month, int day, int hour,
                               int minute, int room, int capacity){
         LocalTime startTime = LocalTime.of(hour, minute);
@@ -46,6 +72,12 @@ public class EventManager implements Serializable {
         }
     }
 
+    /**
+     * This method checks that the room with the given id is available at the specified time.
+     * @param eventTime the time the room's availability is being checked at.
+     * @param room the room id belonging to the room whose availability is being checked.
+     * @return a boolean value; true signalling the room is available at the given time and false if it is not.
+     */
     public boolean freeRoomCheck(ArrayList<LocalDateTime> eventTime, int room){
         LocalDateTime newEventStart = eventTime.get(0);
         LocalDateTime newEventEnd = eventTime.get(1);
@@ -70,6 +102,12 @@ public class EventManager implements Serializable {
         return true;
     }
 
+    /**
+     * This method checks the availability of the speaker whose username is provided at the time that is provided.
+     * @param eventTime the time the speaker's availability is being checked.
+     * @param speaker the username of the speaker whose availability is being checked.
+     * @return a boolean value where true signals the speaker is free at the specified time and false if they are not.
+     */
     public boolean freeSpeakerCheck(ArrayList<LocalDateTime> eventTime, String speaker){
         LocalDateTime newEventStart = eventTime.get(0);
         LocalDateTime newEventEnd = eventTime.get(1);
@@ -90,6 +128,12 @@ public class EventManager implements Serializable {
         return true;
     }
 
+    /**
+     * Method checking that a name is available and can be assigned to a new event.
+     * @param title the event name whose availability is being checked.
+     * @return a boolean value where true signifies the event name is available and false signifies that it is already
+     * in use.
+     */
     public boolean freeTitleCheck(String title) {
         for (Event eventInstance : events) {
             if (eventInstance.getTitle().equals(title)) {
@@ -99,21 +143,47 @@ public class EventManager implements Serializable {
         return true;
     }
 
+    /**
+     * This method returns a list of attendee usernames corresponding to the Attendees attending the specified event.
+     * @param event the event whose list of Attendee usernames will be returned.
+     * @return an Arraylist of Attendee usernames who are attending the provided Event.
+     */
     public ArrayList<String> eventToAttendees(Event event) {
         return event.getAttendeeList();
     }
 
-    // should I add an error message if at capacity?
-    public void signUP(Event event, String attendee){
+    /**
+     * Method adds an Attendee to an Event provided the event is not at capacity. Returns an integer signifying a
+     * successful sign up (0) or that the event is full(1).
+     * @param event the event an Attendee is attempting to be signed up for.
+     * @param attendee the Attendee attempting to be signed up for the given event.
+     * @return a boolean signifying a successful sign up (true) or the event being at capacity (false).
+     */
+    public boolean signUP(Event event, String attendee){
         if(event.getAttendeeList().size() < event.getCapacity()){
             event.addAttendee(attendee);
+            // Attendee successfully signed up
+            return true;
         }
+        // Could not add attendee, event is full.
+        return false;
     }
 
+    /**
+     * This method removes the specified Attendee from the specified event.
+     * @param event the event the Attendee will no longer be attending.
+     * @param attendee the username of the Attendee being removed from the event.
+     */
     public void dropOut(Event event, String attendee){
         event.getAttendeeList().remove(attendee);
     }
 
+    /**
+     * This method changes the speaker to the specified new speaker at the specified event.
+     * @param event the event whose speaker is being changed.
+     * @param speaker the username of the new speaker at the event.
+     * @return a boolean where true signals a successful change and false signals the new speaker is unavailable.
+     */
     public boolean changeSpeaker(Event event, String speaker){
         // checking that the corresponding speaker is free at this event time
         if(freeSpeakerCheck(event.getEventTime(), speaker)){
@@ -123,6 +193,10 @@ public class EventManager implements Serializable {
         return false;
     }
 
+    /**
+     * This method removes the specified event from the list of events at the Tech Conference.
+     * @param event the event to be removed.
+     */
     public void cancelEvent(Event event){
         events.remove(event);
     }
