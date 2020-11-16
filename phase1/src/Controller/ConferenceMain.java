e1a266680de56015e12cd24d5872faf698c96efe
sunnjust <justin.sunn@mail.utoronto.ca>
2020-11-16 17:12:41 -0500

    Added Javadoc comment to ConferenceMain

--- phase1/src/Controller/ConferenceMain.java
+++ phase1/src/Controller/ConferenceMain.java
@@ -1,5 +1,8 @@
 package Controller;
 
+/**
+ * Contains main method that when run starts the program
+ */
 public class ConferenceMain {
     public static void main(String[] args){
         Controller conferenceController = new Controller();
