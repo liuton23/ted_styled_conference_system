ada1cc1827ea82671ee0d4c161e54979f585a03e
Ana Garich <ana.garich@mail.utoronto.ca>
2020-11-30 16:47:56 -0500

    Created very basic VIP event classes. Added a few lines to some methods in sign up system so they can handle the change in EventManager (from an ArrayList of events to a hashmap of event names to events).

--- phase2/src/Entities/VipSpeakerEvent.java
+++ phase2/src/Entities/VipSpeakerEvent.java
@@ -2,6 +2,9 @@ package Entities;
 
 import java.util.ArrayList;
 
+/**
+ * An instance of this class represents a VIP only event with speakers.
+ */
 public class VipSpeakerEvent extends SpeakerEvent {
     public VipSpeakerEvent(String title, ArrayList<String> speaker, int year, String month, int day, int hour, int minute, int room, int duration){
         super(("VIP" + title), speaker, year, month, day, hour, minute, room, duration);
