fd6cc6df03d32a38bda5565d21a3e50cc0cc96b3
sunnjust <justin.sunn@mail.utoronto.ca>
2020-12-09 14:23:45 -0500

    Added ScheduleDownloader class for generating PDFs of conference schedule. Note: requires iText library dependency.

---
+++ phase2/src/Controller/ScheduleDownloader.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class ScheduleDownloader {
+
+    public ScheduleDownloader() {
+
+    }
+
+    private void generatePDF() {
+
+    }
+}
